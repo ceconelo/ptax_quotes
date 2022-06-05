2ce774bca9d3ea3edf286a91414478287083c89b
Thiago Oliveira <thiceconelo@gmail.com>
2022-06-05 13:57:08 -0300

    Change seconds sleep on countdown method

--- ptax/core/utils.py
+++ ptax/core/utils.py
@@ -7,6 +7,6 @@ def countdown(num_of_secs):
         m, s = divmod(num_of_secs, 60)
         sys.stdout.write("\rWe will try again in: {:02d}:{:02d}".format(m, s))
         sys.stdout.flush()
-        sleep(1)
-        num_of_secs -= 1
+        sleep(60)
+        num_of_secs -= 60
     print('\n')
